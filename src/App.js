87eb015aa5f9cb05b0be15816892b7dd2c5ea3ce
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-06-09 14:29:46 +0500

    API_URL Made dynamic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import Ratings from './components/Ratings'
 import ThankYou from './components/ThankYou'
 import axios from 'axios'
+const API_URL = process.env.REACT_APP_API_URL
 
 function App() {
   const [isClicked, setClick] = useState(false)
   const ratingOptions = [1, 2, 3, 4, 5]
   const clicked = async (e, value) => {
     setClick(true)
-    await axios.post('https://rating-system-api.onrender.com/results', {
+    await axios.post(`${API_URL}/results`, {
       resultNumber: value,
     })
   }
